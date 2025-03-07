8bf13a6981cac065c07abfbe929fb5cae4844b2e
AunopDev <s6452410004@sau.ac.th>
2025-03-07 11:50:03 +0700

    Update 07/03/2568

---
+++ routes/run.route.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const runCtrl = require("./../controllers/run.controller.js");
+const run = require("../models/run.model.js");
+
+const router = express.Router();
+
+router.post("/", runCtrl.uploadRun, runCtrl.addRun); //เพิ่มข้อมูลการวิ่ง
+router.put("/:runId", runCtrl.uploadRun, runCtrl.editRun); //แก้ไขข้อมูลการวิ่ง
+router.delete("/:runId", runCtrl.delRun); //ลบข้อมูลการวิ่ง
+router.get("/:runnerId", runCtrl.getAllRun); //ดึงข้อมูลการวิ่งทั้งหมดของนักวิ่งคนนั้นๆ
+router.get("/only/:runId", runCtrl.getOnlyRun); //ดึงข้อมูลการวิ่งหนึ่งๆของคนนั้นๆ
+
+module.exports = router;
